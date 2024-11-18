3cf7f313c3bdd4ae67654011e66016d8e92fe476
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-11-18 12:07:38 +0100

    Add a logger of http requests

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+// const logger = require("morgan");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
@@ -8,6 +8,7 @@ dotenv.config();
 
 require("./models");
 
+const logger = require("./middlewares/logger");
 const indexRouter = require("./routes/index");
 const typeRouter = require("./routes/type");
 const taskRouter = require("./routes/task");
@@ -15,7 +16,7 @@ const legacyTaskRouter = require("./routes/legacy_task");
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -25,5 +26,9 @@ app.use("/", indexRouter);
 app.use("/types", typeRouter);
 app.use("/tasks", taskRouter);
 app.use("/legacy/tasks", legacyTaskRouter);
+
+app.use((req, res) => {
+  res.status(404).send("Not Found");
+});
 
 module.exports = app;
---
+++ middlewares/logger.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+
+const logger = (req, res, next) => {
+  const logDirPath = path.join(__dirname, "../logs");
+
+  if (!fs.existsSync(logDirPath)) {
+    fs.mkdirSync(logDirPath);
+  }
+
+  const logFileName = `${new Date().toISOString().split("T")[0]}.log`;
+  const logFilePath = path.join(logDirPath, logFileName);
+
+  res.on("finish", () => {
+    const logMessage = `${req.method} ${
+      res.statusCode
+    } ${new Date().toISOString()} ${req.originalUrl}\n`;
+
+    fs.appendFile(logFilePath, logMessage, (err) => {
+      if (err) {
+        console.error("Failed to write to log file:", err);
+      }
+    });
+  });
+
+  next();
+};
+
+module.exports = logger;
